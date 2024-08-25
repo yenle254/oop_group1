294b370f96e908cacf110aa98925ee88d4c0bc65
enkhongngoo <yenle252004@gmail.com>
2024-08-26 02:29:36 +0700

    lecture

---
+++ week1/Assignment1.java
@@ -0,0 +1,12 @@
+public class Assignment1 {
+    public static void main(String[] args) {
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        n1.i = n2.i;
+        n2.i = 10; // what is n1.i?
+
+        System.out.println("n1.i = " + n1.i); 
+    }
+}
---
+++ week1/PassObject.java
@@ -0,0 +1,12 @@
+public class PassObject {
+    static void f(Number m) {
+        m.i = 15;
+    }
+
+    public static void main(String[] args) {
+        Number n = new Number();
+        n.i = 14;
+        f(n); 
+        System.out.println("n.i = " + n.i); 
+    }
+}
